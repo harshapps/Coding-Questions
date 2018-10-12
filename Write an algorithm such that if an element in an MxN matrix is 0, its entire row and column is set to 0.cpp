92ded81de1bfe1f04b7b0e5847d10fb75e679e4d
harshapps <harshapps1@gmail.com>
2018-10-12 01:00:08 -0500

    Adding set rows and columns to 0 if 0 occurs program

---
+++ Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column is set to 0.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main(){	
+	int array[4][4] = {{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 0, 1, 1}, {1, 1, 1, 1}}, i, j;		
+	int rowLength = sizeof(array)/sizeof(array[0]), columnLength = sizeof(array[0])/sizeof(array[0][0]);			
+	int row[rowLength], column[columnLength];	
+	
+	cout<<"Before: "<<endl;
+	for(i=0; i<4; i++){
+		for(j=0;j<4;j++){
+			cout<<array[i][j];
+		}cout<<endl;
+	}
+	
+	for(i=0;i<rowLength;i++){
+		for(j=0;j<columnLength;j++){
+			if(array[i][j]==0){
+				row[i]=1;
+				column[j]=1;
+			}				
+		}
+	}
+	
+	for(i=0;i<rowLength;i++){
+		for(j=0;j<columnLength;j++){
+			if(row[i]==1 || column[j]==1)
+				array[i][j]=0;			
+		}
+	}
+	
+	cout<<"\nAfter: "<<endl;
+	for(i=0; i<4; i++){
+		for(j=0;j<4;j++){
+			cout<<array[i][j];
+		}cout<<endl;
+	}
+	return 0;
+}
